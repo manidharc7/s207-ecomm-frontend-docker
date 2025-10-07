91cd724674836a90102e98a8bca2709a386a1dcc
CHALEMCHERLA DURGA MANIDHAR REDDY <manidharc@gmail.com>
2025-10-07 11:56:06 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://3.111.49.199:8083/back1/api/products";  // Updated Backend API URL
+const API_BASE_URL = "http://localhost:80083/back1/api/products";  // Updated Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
